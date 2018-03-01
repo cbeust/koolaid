8e404d2ababeda4ce3c5661173c0a4d00774aa63
Cedric Beust <cedric@beust.com>
2018-03-01 12:23:00 -0800

    Test.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
  "scripts": {
-    "start" : "java -Ddw.server.connector.port=$PORT -jar build/libs/koolaid-1.0-SNAPSHOT-all.jar"
+    "start" : "java -Ddw.server.applicationConnectors[0].port=$PORT -jar build/libs/koolaid-1.0-SNAPSHOT-all.jar"
  }
 
