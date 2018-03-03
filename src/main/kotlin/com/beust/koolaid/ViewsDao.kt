cc807f719688b3bf9e37aa01c4edb696dd35576e
Cedric Beust <cedric@beust.com>
2018-03-02 16:27:05 -0800

    sslmode=require

--- src/main/kotlin/com/beust/koolaid/ViewsDao.kt
+++ src/main/kotlin/com/beust/koolaid/ViewsDao.kt
@@ -1,8 +1,8 @@
 package com.beust.koolaid
 
 import java.net.URI
-import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.Connection
 
 interface ViewsDao {
     fun getViewCountAndIncrement(): Int
@@ -19,7 +19,7 @@ class ViewsDaoPostgres : ViewsDao {
 
         val username = dbUri.userInfo.split(":")[0]
         val password = dbUri.userInfo.split(":")[1]
-        val dbUrl = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path + "?ssl=true"
+        val dbUrl = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path + "?sslmode=require"
 
         println("DATABASE_URL: $databaseUrl")
         println("dbUrl: $dbUrl")
