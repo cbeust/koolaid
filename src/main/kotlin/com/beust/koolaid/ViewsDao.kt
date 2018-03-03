afbe4734815ba35cda8b9e119dec9f2d52f836c8
Cedric Beust <cedric@beust.com>
2018-03-02 16:13:01 -0800

    Log JDBC.

--- src/main/kotlin/com/beust/koolaid/ViewsDao.kt
+++ src/main/kotlin/com/beust/koolaid/ViewsDao.kt
@@ -1,5 +1,6 @@
 package com.beust.koolaid
 
+import java.net.URI
 import java.sql.Connection
 import java.sql.DriverManager
 
@@ -11,7 +12,23 @@ class ViewsDaoPostgres : ViewsDao {
     private val conn: Connection
     init {
         Class.forName("org.postgresql.Driver")
-        conn = DriverManager.getConnection("jdbc:postgresql:demo", "guest", "guest")
+
+        // postgres://{user}:{password}@{hostname}:{port}/{database-name}
+        val databaseUrl = System.getenv("DATABASE_URL")
+        val dbUri = URI(databaseUrl ?: "postgres://guest:guest@localhost:5432/demo")
+
+        val username = dbUri.userInfo.split(":")[0]
+        val password = dbUri.userInfo.split(":")[1]
+        val dbUrl = "jdbc:postgresql://" + dbUri.host + ':' + dbUri.port + dbUri.path + "?sslmode=require"
+
+        println("DATABASE_URL: $databaseUrl")
+        println("dbUrl: $dbUrl")
+        conn = DriverManager.getConnection(dbUrl, username, password)
+
+//        val url = System.getenv("JDBC_DATABASE_URL") ?: "jdbc:postgresql:demo"
+//        val user = System.getenv("JDBC_DATABASE_USER") ?: "guest"
+//        val password = System.getenv("JDBC_DATABASE_PASSWORD") ?: "guest"
+//        conn = DriverManager.getConnection(url, user, password)
     }
 
     override fun getViewCountAndIncrement(): Int {
